56652b720cffa4174ba0a66458b7f9c754d46bbf
Edward Kirakosyan <kirakosyan3010@gmail.com>
2024-01-18 13:21:52 +0400

    c

--- src/app/demo/demo.component.ts
+++ src/app/demo/demo.component.ts
@@ -1,11 +1,11 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'app-demo',
   templateUrl: './demo.component.html',
   styleUrl: './demo.component.scss'
 })
-export class DemoComponent {
+export class DemoComponent implements OnChanges {
   title: string = 'Demo comp'
   @Input() message: string;
 
@@ -13,5 +13,11 @@ export class DemoComponent {
     console.log('demo constractor called')
     console.log(this.title)
     console.log(this.message)
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log('ngOnCHanges Called!!!!')
+    console.log(changes)
+
   }
 }
