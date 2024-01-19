8afdb750080574ecb6941991d9ff474d7276e635
Edward Kirakosyan <kirakosyan3010@gmail.com>
2024-01-19 16:27:19 +0400

    c

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,27 +1,17 @@
-import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup } from "@angular/forms";
+import { Component, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   styleUrls: ['./app.component.scss'],
   template: `
-<form [formGroup]="form">
-  <input type="text" formControlName="first">
-  <input type="text" formControlName="last">
-  <input type="email" formControlName="mail">
-</form>
+    <div>
+      
+    </div>
   `
 })
-export class AppComponent implements OnInit {
-  form
-
-  ngOnInit() {
-    this.form = new FormGroup({
-      first: new FormControl('ss'),
-      last: new FormControl('dd'),
-      mail: new FormControl('ff')
-    })
-  }
+export class AppComponent {
 
 }
+
 
